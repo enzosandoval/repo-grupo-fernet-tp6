e182fb4416263070d0de7795cff6e537a54ab064
Enzo Aramayo <enzosandoval@hotmail.com>
2021-05-19 19:41:46 -0300

    Subida del proyecto

---
+++ repo-grupo-fernet-tp6/src/main/java/ar/edu/unju/fi/RepoGrupoFernetTp6Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoGrupoFernetTp6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoGrupoFernetTp6Application.class, args);
+	}
+
+}
